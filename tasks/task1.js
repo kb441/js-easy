cabd3927dcbed26857062911620dc7391730ef50
kb441 <warmstarjp@gmail.com>
2024-09-18 17:10:26 +0200

    YOUR COMMIT

--- tasks/task1.js
+++ tasks/task1.js
@@ -2,7 +2,20 @@
 // на певний символ, наприклад *.
 
 function replaceVowels(str) {
-  // Ваш код тут
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let result = '';
+
+  // Проходимо по кожному символу рядка
+  for (let i = 0; i < str.length; i++) {
+    // Якщо символ є голосною, замінюємо на '*', інакше залишаємо його
+    if (vowels.includes(str[i].toLowerCase())) {
+      result += '*';
+    } else {
+      result += str[i];
+    }
+  }
+  
+  return result;
 }
 
 console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
